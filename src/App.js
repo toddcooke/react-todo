9831df1a008984bbd1eeec9bbed392b9d96aaac0
toddcooke <toddcookevt@gmail.com>
2021-10-27 14:16:26 -0400

    create todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const todoRepository = {
+    todoCount: 0,
+    todos: [],
+    addTodo(text) {
+        this.todos.push({text: text, id: this.todoCount})
+        this.todoCount += 1
+    },
+    getTodos() {
+        return this.todos
+    },
+    deleteTodo(id) {
+        this.todos = this.todos.filter(it => it.id !== id)
+    }
 }
 
-export default App;
+function TodoApp() {
+    const [todoInputText, setTodoInputText] = useState('')
+    const [todos, setTodos] = useState([])
+
+    function createTodo() {
+        if (todoInputText){
+            todoRepository.addTodo(todoInputText)
+        }
+        setTodoInputText('')
+        setTodos(todoRepository.getTodos())
+    }
+
+    function deleteTodo(id) {
+        todoRepository.deleteTodo(id)
+        setTodos(todoRepository.getTodos())
+    }
+
+    return (
+        <>
+            <h1>Todo list</h1>
+            <button onClick={createTodo}>Create todo</button>
+            <form onSubmit={(e) => {
+                e.preventDefault()
+                createTodo()
+            }}>
+                <input value={todoInputText}
+                       onChange={e => setTodoInputText(e.target.value)}
+                />
+            </form>
+            <ul>
+                {
+                    todos.map(todo => (
+                        <li key={todo.id}>
+                            <button onClick={() => deleteTodo(todo.id)}>
+                                🗑️
+                            </button>
+                            {todo.text}
+                        </li>
+                    ))
+                }
+            </ul>
+        </>
+    )
+}
+
+export default TodoApp;
